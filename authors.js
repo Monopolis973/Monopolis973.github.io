ccd8003e55ab13b3b54df593ac755f25ac725b5b
Spencer Hill <monopolis973@gmail.com>
2024-06-27 21:36:15 -0700

    modified:   authors.js 	modified:   basicpoempage/scripts/main.js 	modified:   charities/charityinfo.js

--- authors.js
+++ authors.js
@@ -1,6 +1,9 @@
 var authors = {
     "Spencer Hill": ["is a student and young poet from California with a heavy interest in creative writing, philosophy, and mathematics. His work has appeared in the Creative Youth Awards, and is currently working on a creative writing anthology with other youth writers in the SF Bay Area.",
-    ["A Mechanic's Apostrophe to Nature"]]
+    [
+        "A Mechanic's Apostrophe to Nature",
+        "[Dis]abled"
+    ]]
 }
 
 
--- charities/charityinfo.js
+++ charities/charityinfo.js
@@ -3,6 +3,11 @@ var charities = {
         'We are dedicated staff, scientists and members advancing effective, lasting conservation in more than 70 countries and territories. To make the highest possible impact on the climate and biodiversity crises between now and 2030, we’re developing breakthrough ideas, amplifying local leaders and influencing policy. There are so many ways to make positive change for our planet. Volunteer with us. Learn how to reduce your carbon footprint. Donate to conservation work.',
         "https://preserve.nature.org/page/80429/donate/1",
         "The Nature Conservancy.svg"
+    ],
+    "Hydrocephalus Association": [
+        `Hydrocephalus is a chronic, neurological condition caused by an abnormal accumulation of cerebrospinal fluid (CSF) within the cavities (ventricles) of the brain. The mission of the Hydrocephalus Association is to find a cure for hydrocephalus and improve the lives of those impacted by the condition. But we can’t do it alone. Help us change the future of hydrocephalus by supporting the Hydrocephalus Association!`,
+        "https://www.hydroassoc.org/",
+        "Hydrocephalus Association.webp"
     ]
 };
 
